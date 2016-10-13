71266bec6712204a37079434730bf8085feca24e
jonas Van Loey <jonas Van Loey>
2016-10-13 16:50:15 +0200

    commit part 2 13/10

--- opdracht-functions-recursive.php
+++ opdracht-functions-recursive.php
@@ -4,16 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php
-        $md5HashKey = 'd1fa402db91a7a93c4f414b8278ce073';
-
-
-
+        function bank($bedrag){
+            $jaar = 1;
+            for ($jaar;$jaar<=10;++$jaar){
+                $nieuwbedrag = $bedrag *pow(1.08,$jaar);
+                echo 'na '.$jaar.' heeft Hans '.$nieuwbedrag.'euro <br>';
+            }
+        }
         ?>
-
     </head>
     <body class="web-backend-opdracht">
         
         <section class="body">
+            <?php bank(100000) ?>
 
         </section>
 
